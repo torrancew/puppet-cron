d2a8299d886508b090cbcc83881d38ea9fb729a9
Kevin G <cpan@goess.org>
2012-10-30 13:01:53 -0700

    passing 'environment' one step further
    Correct me if I'm wrong, but it looks like we missed a setp, we need to list
    'environment' in the argument list when we call "cron::job { $name:...", at least
    I needed to do this to get it working.

--- manifests/weekly.pp
+++ manifests/weekly.pp
@@ -1,13 +1,14 @@
 define cron::weekly( $minute = 0, $hour = 0, $weekday = 0, $environment = [], $user = 'root', $command ) {
   cron::job {
     $name:
-      minute  => $minute,
-      hour    => $hour,
-      date    => '*',
-      month   => '*',
-      weekday => $weekday,
-      user    => $user,
-      command => $command;
+      minute      => $minute,
+      hour        => $hour,
+      date        => '*',
+      month       => '*',
+      weekday     => $weekday,
+      user        => $user,
+      environment => $environment,
+      command     => $command;
   }
 }
 
