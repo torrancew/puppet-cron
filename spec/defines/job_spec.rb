c56bbc0a52944a60df7ad025ee8b11f582f0f588
Tray Torrance <devwork@warrentorrance.com>
2012-11-15 20:12:11 -0800

    Significantly enhance the 'cron::job' type spec.
    Now tests the following:
      * Environment settings
      * Proper Cron-formatted timestamp
      * User
      * Command

--- spec/defines/job_spec.rb
+++ spec/defines/job_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 describe 'cron::job' do
   let( :title )  { 'mysql_backup' }
   let( :params ) {{
-    :environment => 'MAILTO="root"',
+    :minute      => '45',
+    :hour        => '7',
+    :date        => '12',
+    :month       => '7',
+    :weekday     => '*',
+    :environment => [ 'MAILTO="root"', 'PATH="/usr/sbin:/usr/bin:/sbin:/bin"' ],
+    :user        => 'root',
     :command     => 'mysqldump -u root test_db >some_file',
   }}
 
   it do
+    cron_timestamp = ""
+    [ :minute, :hour, :date, :month, :weekday ].each do |k|
+      cron_timestamp << "#{params[k]} "
+    end
+    cron_timestamp.strip!
+
     should contain_file( "job_#{title}" ).with(
       'ensure'  => 'file',
       'owner'   => 'root',
@@ -15,9 +27,13 @@ describe 'cron::job' do
       'mode'    => '0640',
       'path'    => "/etc/cron.d/#{title}",
     ).with_content(
-      /#{params[:environment]}/
+      /\n#{params[:environment].join('\n')}\n/
     ).with_content(
-      /#{params[:command]}/
+      /\n#{cron_timestamp}\s+/
+    ).with_content(
+      /\s+#{params[:user]}\s+/
+    ).with_content(
+      /\s+#{params[:command]}\n/
     )
   end
 end
