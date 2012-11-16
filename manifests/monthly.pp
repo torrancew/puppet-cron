ed605c61cbde8dc98624443ae12d940b8468d17e
Tray Torrance <devwork@warrentorrance.com>
2012-11-15 19:56:38 -0800

    Add specs for the 'cron::monthly' type.

--- manifests/monthly.pp
+++ manifests/monthly.pp
@@ -1,6 +1,6 @@
 define cron::monthly( $minute = 0, $hour = 0, $date = 1, $environment = [], $user = 'root', $command ) {
   cron::job {
-    $name:
+    $title:
       minute      => $minute,
       hour        => $hour,
       date        => $date,
