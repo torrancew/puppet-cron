4c79c19e53f3febc3d9dbf1a2810a80aac193960
Tray Torrance <devwork@warrentorrance.com>
2012-11-17 14:29:24 -0800

    Update cron::install test

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe 'cron::install' do
   it do
-    should contain_package( 'cron' ).with(
-      'ensure' => 'installed',
-    )
+    should contain_package( 'cron' ).with( 'ensure' => 'installed' )
   end
 end
 
