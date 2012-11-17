85b677918d21a65c02708ae084ae1ee78b8e0ca4
Tray Torrance <devwork@warrentorrance.com>
2012-11-17 10:31:36 -0800

    Add PuppetDoc for the 'cron' and 'cron::install' classes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,17 @@
+# Class: puppet
+#
+# This class wraps *cron::instalL* for ease of use
+#
+# Parameters:
+# 
+# Actions:
+# 
+# Requires:
+#
+# Sample Usage:
+#   include 'cron'
+#   class { 'cron': }
+
 class cron {
   include cron::install
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,17 @@
+# Class: cron::install
+#
+# This class ensures that the distro-appropriate cron package is installed
+# 
+# Parameters:
+# 
+# Actions:
+# 
+# Requires:
+# 
+# Sample Usage:
+#   This class should not be used directly under normal circumstances
+#   Instead, use the *cron* class.
+
 class cron::install {
   $package_name = $operatingsystem ? {
     /(RedHat|CentOS|OracleLinux)/ => 'cronie',
