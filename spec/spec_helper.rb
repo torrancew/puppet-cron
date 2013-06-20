b955693ed4a60a01aeb8ea31c5c80332f43821b4
Tray Torrance <devwork@warrentorrance.com>
2013-06-20 09:12:51 -0700

    Convert test fixtures over to new style provided by PL - fixes #12
    Test fixtures are now generated as per
    https://puppetlabs.com/blog/the-next-generation-of-puppet-module-testing/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
-require 'rspec-puppet'
-
-fixture_path = File.expand_path( File.join( __FILE__, '..', 'fixtures' ))
-
-RSpec.configure do |spec|
-  spec.module_path  = File.join( fixture_path, 'modules' )
-  spec.manifest_dir = File.join( fixture_path, 'manifests' )
-end
+require 'rubygems'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 def get_timestamp( params = {} )
   stamp = ''
