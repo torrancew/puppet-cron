2d8db4fe6cd802405ddc62e91f6a2b7851830946
Tray Torrance <devwork@warrentorrance.com>
2013-10-07 00:09:37 -0700

    Support Amazon Linux - fixes #19

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,21 @@
 # Class: cron::install
 #
 # This class ensures that the distro-appropriate cron package is installed
-# 
+#
 # Parameters:
-# 
+#
 # Actions:
-# 
+#
 # Requires:
-# 
+#
 # Sample Usage:
 #   This class should not be used directly under normal circumstances
 #   Instead, use the *cron* class.
 
 class cron::install {
-  $package_name = $operatingsystem ? {
-    /(RedHat|CentOS|OracleLinux)/ => 'cronie',
-    default                       => 'cron',
+  $package_name = $::operatingsystem ? {
+    /(RedHat|CentOS|Amazon|OracleLinux)/ => 'cronie',
+    default                              => 'cron',
   }
 
   package {
