6960d258807514ab91ae1278fa19d53d1793bd8d
Diddi Oscarsson <diddi.oscarsson@cyberbacon.net>
2013-10-30 11:08:03 +0100

    Add support for hiera
    This is to make it possible to easily define jobs in hiera.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,5 +14,30 @@
 
 class cron {
   include cron::install
+
+  $jobs = hiera_hash('cron::job', undef)
+  if $jobs != undef {
+    create_resources('cron::job', $jobs)
+  }
+
+  $daily = hiera_hash('cron::daily', undef)
+  if $daily != undef {
+    create_resources('cron::daily', $daily)
+  }
+
+  $weekly = hiera_hash('cron::weekly', undef)
+  if $weekly != undef {
+    create_resources('cron::weekly', $weekly)
+  }
+
+  $monthly = hiera_hash('cron::monthly', undef)
+  if $monthly != undef {
+    create_resources('cron::monthly', $monthly)
+  }
+
+  $hourly = hiera_hash('cron::hourly', undef)
+  if $hourly != undef {
+    create_resources('cron::hourly', $hourly)
+  }
 }
 
