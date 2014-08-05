3f2883f2404205f47bac3b000677d8f3ffd03b3e
Shaun Martin <shaunm@airg.com>
2014-08-05 16:23:16 +0000

    Finish code for multiple jobs in a single file.

--- manifests/multiple.pp
+++ manifests/multiple.pp
@@ -2,26 +2,24 @@ define cron::multiple
 (
     $crond_path     = '/etc/cron.d',
     $environment    = [],
+	$mode           = 0644,
     $jobs,
 )
 {
-
-    case $ensure{
-        'present': { $real_ensure = file }
-        'absent': { $real_ensure = absent }
-        default: { fail("Invalid value '${ensure}' used for ensure") }
-    }
 
     $file = "${crond_path}/${title}"
     $job_defaults = { 
         'file'          => $file,
     }
 
-    concat { "${file}" : }
+    concat { "${file}" : 
+        mode    => $mode
+    }
 
     concat::fragment { "${title}_cron_header" :
         target  => $file,
         content => template('cron/fragment_header.erb'),
+        order   => '1',
     }
 
     create_resources('cron::multiple::job', $jobs, $job_defaults)
@@ -38,12 +36,11 @@ define cron::multiple::job
 	$month = '*',
 	$weekday = '*',
 	$user = 'root',
-	$mode = 0644,
-	$ensure = 'present',
 )
 {
     concat::fragment { "${title}_cron" :
         target  => $file,
         content => template('cron/fragment_job.erb'),
+        order   => '2',
     }
 }
