c7684f38d4de6120768a3997848613e936be7863
Tomasz Struczyński <tomasz.struczynski@socializer.pl>
2015-05-05 18:06:11 +0200

    Added a possibility of Hiera based configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,19 @@
 #   include 'cron'
 #   class { 'cron': }
 
-class cron {
+class cron (
+  $jobs = {},
+  $daily = {},
+  $hourly = {},
+  $monthly = {},
+  $weekly = {},
+)  {
   include cron::install
+
+  create_resources('cron::job', $jobs)
+  create_resources('cron::daily', $daily)
+  create_resources('cron::hourly', $hourly)
+  create_resources('cron::monthly', $monthly)
+  create_resources('cron::weekly', $weekly) 
 }
 
