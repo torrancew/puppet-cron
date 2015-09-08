34e534fdb06249e15e0105d1660063feaa49d2a8
Matthew Gyurgyik <gyurgyikms@ornl.gov>
2015-09-08 15:20:34 -0400

    store file mode as a string
    This allows for puppet4 support.
    
    "The mode attribute of a file resource must be a string. If you use an
    actual octal number, it will be converted to a decimal number, then
    converted back to a string representing the wrong number when it comes
    time to run the chown command."
    
    https://docs.puppetlabs.com/puppet/latest/reference/experiments_future.html#check-the-mode-attribute-of-any-file-resources

--- manifests/monthly.pp
+++ manifests/monthly.pp
@@ -35,7 +35,7 @@
 
 define cron::monthly(
   $command, $minute = 0, $hour = 0, $date = 1,
-  $environment = [], $user = 'root', $mode = 0644, $ensure = 'present'
+  $environment = [], $user = 'root', $mode = '0644', $ensure = 'present'
 ) {
   cron::job {
     $title:
